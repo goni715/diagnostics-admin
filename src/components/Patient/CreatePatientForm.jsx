9c11153d44038963735cf22f86d5e228468a3a80
goni715 <halesgoni715149123@gmail.com>
2024-09-24 09:34:40 +0600

    search-feater added

--- src/components/Patient/CreatePatientForm.jsx
+++ src/components/Patient/CreatePatientForm.jsx
@@ -72,7 +72,7 @@ const CreatePatientForm = () => {
     return (
         <>
             <form onSubmit={handleSubmit} className="px-3 md:px-6 pb-6 shadow-md rounded md:rounded-md">
-                <h1 className="text-center text-2xl font-bold mb-10">Create New Invoice</h1>
+                <h1 className="text-center text-2xl font-bold mb-10">Create New Patient</h1>
                 <h3 className="text-2xl my-3">Patient Details:</h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     <div>
